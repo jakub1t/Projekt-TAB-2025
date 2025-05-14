39d890e1f777512e56bc7f40092071f6b4d01b0c
VX1-AEiI <114992080+VX1-AEiI@users.noreply.github.com>
2025-05-14 22:01:21 +0200

    Login works!

--- FirmaKurierska/src/main/java/com/polsl/firmakurierska/view/hello_world/Hello_world.java
+++ FirmaKurierska/src/main/java/com/polsl/firmakurierska/view/hello_world/Hello_world.java
@@ -4,6 +4,15 @@
 
 package com.polsl.firmakurierska.view.hello_world;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
@@ -29,6 +38,19 @@ public class Hello_world extends Application {
         loguj.setOnAction(e -> {
             System.out.println("Nick: " + nick.getText());
             System.out.println("Hasło: " + haslo.getText());
+
+            boolean loginSuccessful = false;
+            try {
+                loginSuccessful = httpLogin(nick.getText(), haslo.getText());
+            } catch (URISyntaxException ex) {
+                System.out.println(ex.toString());
+                ex.printStackTrace();
+            }
+
+
+            if (loginSuccessful) {
+                new AdminPanel().start(new Stage());
+            }
             
             if(nick.getText().equals("a")) {
                 new AdminPanel().start(new Stage());
@@ -36,6 +58,10 @@ public class Hello_world extends Application {
             
             if(nick.getText().equals("w")) {
                 new WorkerAdmin().start(new Stage());
+            }
+            
+            if(nick.getText().equals("k")) {
+                new WorkerWindow().start(new Stage());
             }
 
             
@@ -57,7 +83,62 @@ public class Hello_world extends Application {
         launch(args);
     }
 
-    //private boolean httpLogin() {
+    private boolean httpLogin(String nick, String pass) throws URISyntaxException {
+        String bazaUrl = "http://localhost:8080/konto/login";
+
+        String reqString = "{\"login\": \"" + nick + "\",\"haslo\": \"" + pass + "\"}";
+
+        System.out.println(reqString);
+
+        boolean success = false;
+
+        try {
+            URL obj = new URI(bazaUrl).toURL();
+
+            HttpURLConnection konnect = (HttpURLConnection)obj.openConnection();
+
+            int responseCode = 0;
+
+            konnect.setRequestMethod("POST");
+            konnect.setDoOutput(true);
+            konnect.setRequestProperty("Content-Type", "application/json");
+
+            try (DataOutputStream os = new DataOutputStream(konnect.getOutputStream())) {
+                os.writeBytes(reqString);
+                os.flush();
+            }
+
+            responseCode = konnect.getResponseCode();
+
+            StringBuilder response = new StringBuilder();
+
+                // Read response content
+                // connection.getInputStream() purpose is to obtain an input stream for reading the server's response.
+                try (
+                    BufferedReader reader = new BufferedReader( new InputStreamReader( konnect.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line); // Adds every line to response till the end of file.
+                    }
+                }
+                System.out.println("Response: " + response.toString());
+
+            if (responseCode == 200) {
+                success = true;
+            }
+
+            konnect.disconnect();
+        }
+        catch (IOException e) {
+            // If any error occurs during api call it will go into catch block
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+        return success;
+
+    }
+
+    //private int httpFindAccType(String nick) {
 
     //}
 }
