1059b9715b76ca66e2d29c5d0cdd4a7d471af1da
jakub1t <cozzrobic6@gmail.com>
2025-05-14 18:54:19 +0200

    First tests with database.

--- FirmaKurierska/src/main/java/com/polsl/firmakurierska/FirmaKurierskaApplication.java
+++ FirmaKurierska/src/main/java/com/polsl/firmakurierska/FirmaKurierskaApplication.java
@@ -1,21 +1,30 @@
 package com.polsl.firmakurierska;
 
-import java.awt.BorderLayout;
+import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JTextArea;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.ResponseEntity;
 
+import com.polsl.firmakurierska.controller.DostawaController;
+import com.polsl.firmakurierska.controller.KlientController;
 import com.polsl.firmakurierska.controller.KontoController;
+import com.polsl.firmakurierska.controller.PaczkaController;
+import com.polsl.firmakurierska.controller.PojazdController;
+import com.polsl.firmakurierska.controller.PracownikController;
+import com.polsl.firmakurierska.controller.PrawoJazdyController;
+import com.polsl.firmakurierska.controller.ProducentController;
+import com.polsl.firmakurierska.controller.ProduktController;
+import com.polsl.firmakurierska.controller.StanowiskoController;
+import com.polsl.firmakurierska.dto.PracownikDTO;
+import com.polsl.firmakurierska.dto.PrawoJazdyDTO;
+import com.polsl.firmakurierska.exception.BadRequestException;
 import com.polsl.firmakurierska.model.Konto;
+import com.polsl.firmakurierska.model.Stanowisko;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -29,6 +38,25 @@ public class FirmaKurierskaApplication implements CommandLineRunner {
 
     @Autowired
     private KontoController kontoController;
+    @Autowired
+    private PracownikController pracownikController;
+    @Autowired
+    private PrawoJazdyController prawoJazdyController;
+    @Autowired
+    private StanowiskoController stanowiskoController;
+
+    @Autowired
+    private PojazdController pojazdController;
+    @Autowired
+    private KlientController klientController;
+    @Autowired
+    private DostawaController dostawaController;
+    @Autowired
+    private PaczkaController paczkaController;
+    @Autowired
+    private ProducentController producentController;
+    @Autowired
+    private ProduktController produktController;
 
 	public static void main(String[] args) {
 		// SpringApplication.run(FirmaKurierskaApplication.class, args);
@@ -40,26 +68,78 @@ public class FirmaKurierskaApplication implements CommandLineRunner {
     public void run(String... args) {
 
 		// Get list of all accounts
-		List<Konto> accounts = kontoController.getAllKonta();
-		// Print logins to console
-		accounts.forEach(account -> System.out.println(account.getLogin()));
+		// List<Konto> accounts = kontoController.getAllKonta();
+		List<Konto> accounts = getAllAccounts();
+		// Print ids to console
+		accounts.forEach(account -> System.out.println(account.getIdKonta()));
 
-		// Window stuff here
-        JFrame frame = new JFrame("Spring Boot Swing App");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        JPanel panel = new JPanel(new BorderLayout());
-		JTextField text = new JTextField("Some text blah bla blah...");
-		panel.add(text, BorderLayout.BEFORE_FIRST_LINE);
+		// ---------------------------------------------------------------------- //
+		// Here some tests for database methods:
 
-		String text_temp = "";
-		for (Konto konto : accounts) {
-			text_temp += "Login: " + konto.getLogin() + " ||| Haslo: " + konto.getHaslo() + "\n\r";
-		}
+		Konto konto = getAccountByLogin("user1");
+		System.out.println(konto.getIdKonta());
 
-		JTextArea textField = new JTextArea(text_temp);
-		panel.add(textField, BorderLayout.CENTER);
-        frame.setContentPane(panel);
-        frame.setVisible(true);
+		List<String> data = getDataForAccount(konto.getIdKonta().toString());
+
+		System.out.println("Imię: " + data.get(0));
+		System.out.println("Nazwisko: " + data.get(1));
+		System.out.println("PESEL: " + data.get(2));
+		System.out.println("Stanowisko: " + data.get(3));
+		System.out.println("Kategoria prawa jazdy: " + data.get(4));
+
+		// deleteAccountByLogin("user2"); // Nie działa
+
+		accounts = getAllAccounts();
+		accounts.forEach(account -> System.out.println(account.getIdKonta()));
+
+		// ---------------------------------------------------------------------- //
     }
+
+	public List<Konto> getAllAccounts() {
+		return this.kontoController.getAllKonta();
+	}
+
+	public Konto getAccountByLogin(String login) {
+		return this.kontoController.getKontoByLogin(login);
+	}
+
+	public void deleteAccountByLogin(String login) {
+		this.kontoController.deleteKonto(login);
+	}
+
+	/**
+	 * Gets worker data into list with order: {name, surname, PESEL, position, dirving license}.
+	 * @param accountId - id of the account that the data is collected.
+	 * @return - account data in a list.
+	 */
+	public List<String> getDataForAccount(String accountId) {
+		List<String> accountData = new ArrayList<>();
+		int accountIdAsInt = 0;
+		try {
+			accountIdAsInt = Integer.parseInt(accountId);
+		} catch (NumberFormatException e) {
+            throw new BadRequestException("ID musi być liczbą całkowitą: " + accountId);
+        }
+
+		// Get worker name, surname and PESEL
+		ResponseEntity<PracownikDTO> pracownik_rpent = pracownikController.getPracownikById(accountId);
+		PracownikDTO pracownik = pracownik_rpent.getBody();
+
+		accountData.add(pracownik.getImie());
+		accountData.add(pracownik.getNazwisko());
+		accountData.add(pracownik.getPesel());
+
+		// Get position
+		Stanowisko stanowisko = stanowiskoController.getStanowiskoById(accountIdAsInt);
+		accountData.add(stanowisko.getNazwaStanowiska());
+
+		// Get driving license
+		ResponseEntity<PrawoJazdyDTO> prawoJazdy_rpent = prawoJazdyController.getPrawoJazdyById(accountId);
+		PrawoJazdyDTO prawoJazdy = prawoJazdy_rpent.getBody();
+
+		accountData.add(prawoJazdy.getKategoria());
+
+		return accountData;
+	}
+
 }
